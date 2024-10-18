18df0fff096189ac9e04a0b25b3c0d1ec3766e4a
Aymen-Guerrouf <154664547+Aymen-Guerrouf@users.noreply.github.com>
2024-10-18 09:22:39 +0000

    copilot second commit

---
+++ members.js
@@ -0,0 +1,16 @@
+function skillsMembers() {
+  return [
+    {
+      name: 'John',
+      skills: ['javascript', 'css', 'html']
+    },
+    {
+      name: 'Jane',
+      skills: ['javascript', 'html']
+    },
+    {
+      name: 'Jack',
+      skills: ['css', 'html']
+    }
+  ];
+}
